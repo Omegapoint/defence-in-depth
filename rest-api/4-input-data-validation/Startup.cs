9b62d45cce0ba77a717d7197031707c959f80bf4
TobiasAhnoff <tobahn@omegapoint.se>
2021-08-16 13:44:25 +0200

    Rename demos to match article updates

---
+++ rest-api/4-input-data-validation/Startup.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Defence.In.Depth
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IClaimsTransformation, ClaimsTransformation>();
+
+            services.AddControllers();
+
+            // Note that this is only demo code, token validation is part of demo 2
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => {
+                    options.Authority = "https://demo.identityserver.io";
+                    options.Audience = "api";
+                });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints
+                    .MapControllers()
+                    .RequireAuthorization();
+            });
+        }
+    }
+}
