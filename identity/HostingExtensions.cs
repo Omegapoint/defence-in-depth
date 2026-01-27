d177247899d01580e36b62b27347903744e51831
Tobias Ahnoff <tobias.ahnoff@omegapoint.se>
2026-01-27 11:24:03 +0100

    Remove IdentityServer UI, base on client-credentials Quickstart

---
+++ identity/HostingExtensions.cs
@@ -0,0 +1,33 @@
+// Copyright (c) Duende Software. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Serilog;
+
+namespace IdentityServer;
+
+internal static class HostingExtensions
+{
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddIdentityServer()
+            .AddInMemoryApiResources(Config.ApiResources)
+            .AddInMemoryApiScopes(Config.ApiScopes)
+            .AddInMemoryClients(Config.Clients);
+
+        return builder.Build();
+    }
+
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        app.UseSerilogRequestLogging();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseIdentityServer();
+
+        return app;
+    }
+}
